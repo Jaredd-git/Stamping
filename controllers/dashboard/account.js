8cbe7243dd0ac6d6f8d5d38844edc7419d47e432
Jaredd-git <20210612@ricaldone.edu.sv>
2023-04-13 10:49:06 -0600

    cambios

--- controllers/dashboard/account.js
+++ controllers/dashboard/account.js
@@ -6,7 +6,7 @@ const FOOTER = document.querySelector('footer');
 HEADER.innerHTML = `
     <!-- Navbar de la pagina web -->
         <div class="sticky-top">
-            <nav class="barra-nav navbar navbar-ligth sticky-top" style="background-color: #ccc">
+            <nav class="barra-nav navbar navbar-ligth sticky-top" style="background-color: #ccc;">
                 <div class="container-fluid">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
@@ -15,9 +15,9 @@ HEADER.innerHTML = `
                     </button>
                     <span class="navbar-brand mb-0 h1">Stamping</span>
                     <section>
-                        <a href="#"><i><img style="width: 40px; " src="../image/iconos/isearch.png"></i></a>
-                        <a href="#"><i><img style="width: 32px;" src="../image/iconos/iuser.png"></i></a>
-                        <a href="#"><i><img style="width: 32px;" src="../image/iconos/ibolsa.png"></i></a>
+                        <a href="#"><i><img style="width: 40px; " src="../../resources/img/iconos/isearch.png"></i></a>
+                        <a href="#"><i><img style="width: 32px;" src="../../resources/img/iconos/iuser.png"></i></a>
+                        <a href="#"><i><img style="width: 32px;" src="../../resources/img/iconos/ibolsa.png"></i></a>
                     </section>
                 </div>
             </nav>
@@ -26,17 +26,17 @@ HEADER.innerHTML = `
                     <ul class="nav flex-row">
                         <div class="flex-column">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="../public/ofertas.html">Productos
+                                <a class="nav-link active" aria-current="page" href="../../views/public/ofertas.html">Productos
                                     en
                                     oferta</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="../public/productos.html">Nuevos diseños</a>
+                                <a class="nav-link" href="../../views/public/productos.html">Nuevos diseños</a>
                             </li>
                         </div>
                         <div class="v-line"></div>
                         <li class="nav-item">
-                            <a class="nav-link" href="../public/nuevosproductos.html">Todos los productos</a>
+                            <a class="nav-link" href="../../views/public/nuevosproductos.html">Todos los productos</a>
                         </li>
                     </ul>
                 </div>
@@ -46,26 +46,26 @@ HEADER.innerHTML = `
 
 /* Se manda al footer codigo para usarlo en el html */
 FOOTER.innerHTML = `
-    <div class="container p-4 pb-0" style="background-color: #cccccc">
+    <div class="container p-4 pb-0" style="background-color: #cccccc; height: 40px;">
         <section class="mb-4 row row-cols-1 row-cols-md-3 g-4">
             <!-- Nombre de la empresa -->
             <a style="text-align: left;" class="text-dark">Stamping Store</a>
             <!-- Enlaces a redes sociales -->
             <section>
-                <a href="https://www.facebook.com/"><i><img style="width: 30px; " src="../image/iconos/Facebook.png"></i></a>
-                <a href="https://www.instagram.com/"><i><img style="width: 30px;" src="../image/iconos/Instagram.png"></i></a>
+                <a href="https://www.facebook.com/"><i><img style="width: 30px; " src="../../resources/img/iconos/Facebook.png"></i></a>
+                <a href="https://www.instagram.com/"><i><img style="width: 30px;" src="../../resources/img/iconos/Instagram.png"></i></a>
                 <a href="https://www.whatsapp.com/?lang=es"><i><img style="width: 30px;"
-                    src="../image/iconos/WhatsApp.png"></i></a>
-                <a href="https://www.pinterest.es/"><i><img style="width: 30px;" src="../image/iconos/Pinterest.png"></i></a>
+                    src="../../resources/img/iconos/Whatsapp.png"></i></a>
+                <a href="https://www.pinterest.es/"><i><img style="width: 30px;" src="../../resources/img/iconos/Pinterest.png"></i></a>
             </section>
              <!-- Enlace a terminos y condiciones -->
             <section style="text-align: right;">
                 <div class="row">
                     <a style="text-align: right;" class="text-dark">Terminos y condiciones</a>
                     <div class="col">
-                        <a style="text-align: right;" class="text-dark" href="../public/sobrenostros.html">Nosotros</a>
+                        <a style="text-align: right;" class="text-dark" href="../../views/public/sobrenostros.html">Nosotros</a>
                         
-                        <a style="text-align: right;" class="text-dark" href="../public/">Contactanos</a>
+                        <a style="text-align: right;" class="text-dark" href="../../views/public/">Contactanos</a>
                     </div>                    
                 </div>             
             </section>
