c1e8c79e94865c7e21a8b46aa51e15ec4bd1350b
Devora Meltrozo <20210612@ricaldone.edu.sv>
2023-04-13 14:49:47 -0600

    cambios

--- controllers/dashboard/account.js
+++ controllers/dashboard/account.js
@@ -5,42 +5,12 @@ const FOOTER = document.querySelector('footer');
 /* Se manda al header codigo para usarlo en el html */
 HEADER.innerHTML = `
     <!-- Navbar de la pagina web -->
-        <div class="sticky-top">
-            <nav class="barra-nav navbar navbar-ligth sticky-top" style="background-color: #ccc;">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <span class="navbar-brand mb-0 h1">Stamping</span>
-                    <section>
-                        <a href="#"><i><img style="width: 40px; " src="../../resources/img/iconos/isearch.png"></i></a>
-                        <a href="#"><i><img style="width: 32px;" src="../../resources/img/iconos/iuser.png"></i></a>
-                        <a href="#"><i><img style="width: 32px;" src="../../resources/img/iconos/ibolsa.png"></i></a>
-                    </section>
-                </div>
-            </nav>
-            <div class="collapse sticky-top" id="navbarToggleExternalContent">
-                <div class="bg-ligth p-4 contenido-navbar opacity-75">
-                    <ul class="nav flex-row">
-                        <div class="flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="../../views/public/ofertas.html">Productos
-                                    en
-                                    oferta</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../../views/public/productos.html">Nuevos diseños</a>
-                            </li>
-                        </div>
-                        <div class="v-line"></div>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../../views/public/nuevosproductos.html">Todos los productos</a>
-                        </li>
-                    </ul>
-                </div>
+    <div class="sticky-top">
+        <nav class="barra-nav navbar-ligth sticky-top" style="background-color: #ccc;">
+            <div class="text-center p-4" style="justify-content: center;">
+                <a class="text-reset fw-bold" href="home.html">STAMPING</a>
             </div>
+        </nav>           
     </div>
     `;
 
