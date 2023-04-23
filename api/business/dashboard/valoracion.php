a704f38d84baa953443aa9ff006439bae7478d6e
Jaredd-git <20210612@ricaldone.edu.sv>
2023-04-23 17:39:59 -0600

    cambios

--- api/business/dashboard/valoracion.php
+++ api/business/dashboard/valoracion.php
@@ -22,58 +22,6 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'Alias de usuario indefinido';
                 }
                 break;
-            case 'logOut':
-                if (session_destroy()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Sesión eliminada correctamente';
-                } else {
-                    $result['exception'] = 'Ocurrió un problema al cerrar la sesión';
-                }
-                break;
-            case 'readProfile':
-                if ($result['dataset'] = $usuario->readProfile()) {
-                    $result['status'] = 1;
-                } elseif (Database::getException()) {
-                    $result['exception'] = Database::getException();
-                } else {
-                    $result['exception'] = 'Usuario inexistente';
-                }
-                break;
-            case 'editProfile':
-                $_POST = Validator::validateForm($_POST);
-                if (!$usuario->setNombres($_POST['nombres'])) {
-                    $result['exception'] = 'Nombres incorrectos';
-                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
-                    $result['exception'] = 'Apellidos incorrectos';
-                } elseif (!$usuario->setCorreo($_POST['correo'])) {
-                    $result['exception'] = 'Correo incorrecto';
-                } elseif (!$usuario->setAlias($_POST['alias'])) {
-                    $result['exception'] = 'Alias incorrecto';
-                } elseif ($usuario->editProfile()) {
-                    $result['status'] = 1;
-                    $_SESSION['alias_admin'] = $usuario->getAlias();
-                    $result['message'] = 'Perfil modificado correctamente';
-                } else {
-                    $result['exception'] = Database::getException();
-                }
-                break;
-            case 'changePassword':
-                $_POST = Validator::validateForm($_POST);
-                if (!$usuario->setId($_SESSION['id_admin'])) {
-                    $result['exception'] = 'Usuario incorrecto';
-                } elseif (!$usuario->checkPassword($_POST['actual'])) {
-                    $result['exception'] = 'Clave actual incorrecta';
-                } elseif ($_POST['nueva'] != $_POST['confirmar']) {
-                    $result['exception'] = 'Claves nuevas diferentes';
-                } elseif (!$usuario->setClave($_POST['nueva'])) {
-                    $result['exception'] = Validator::getPasswordError();
-                } elseif ($usuario->changePassword()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Contraseña cambiada correctamente';
-                } else {
-                    $result['exception'] = Database::getException();
-                }
-                break;
             case 'readAll':
                 if ($result['dataset'] = $valoracion->readAll()) {
                     $result['status'] = 1;
@@ -97,37 +45,8 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'No hay coincidencias';
                 }
                 break;
-            case 'create':
-                $_POST = Validator::validateForm($_POST);
-                if (!$cliente->setNombres($_POST['nombres'])) {
-                    $result['exception'] = 'Nombres incorrectos';
-                } elseif (!$cliente->setApellidos($_POST['apellidos'])) {
-                    $result['exception'] = 'Apellidos incorrectos';
-                } elseif (!$cliente->setDUI($_POST['DUI'])) {
-                    $result['exception'] = 'DUI incorrecto';
-                } elseif (!$cliente->setCorreo($_POST['correo'])) {
-                    $result['exception'] = 'Correo incorrecto';
-                } elseif (!$cliente->setTelefono($_POST['telefono'])) {
-                    $result['exception'] = 'Telefono incorrecto';
-                } elseif (!$cliente->setNacimiento($_POST['nacimiento'])) {    
-                    $result['exception'] = 'Fecha incorrecto';
-                } elseif (!$cliente->setDireccion($_POST['direccion'])) {
-                    $result['exception'] = 'Direccion incorrecto';
-                } elseif (!$cliente->setUser($_POST['user'])) {
-                    $result['exception'] = 'Usuario incorrecto';
-                } elseif ($_POST['clave'] != $_POST['confirmar']) {
-                    $result['exception'] = 'Claves diferentes';
-                } elseif (!$usuario->setClave($_POST['clave'])) {
-                    $result['exception'] = Validator::getPasswordError();
-                } elseif ($usuario->createRow()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Cliente creado correctamente';
-                } else {
-                    $result['exception'] = Database::getException();
-                }
-                break;
             case 'readOne':
-                if (!$valoracion->setId($_POST['id_valoracion'])) {
+                if (!$valoracion->setId($_POST['id'])) {
                     $result['exception'] = 'Valoracion incorrecta';
                 } elseif ($result['dataset'] = $valoracion->readOne()) {
                     $result['status'] = 1;
@@ -137,35 +56,8 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'Valoracion inexistente';
                 }
                 break;
-            case 'update':
-                $_POST = Validator::validateForm($_POST);
-                if (!$cliente->setId($_POST['id'])) {
-                    $result['exception'] = 'Cliente incorrecto';
-                } elseif (!$cliente->readOne()) {
-                    $result['exception'] = 'Cliente inexistente';
-                } elseif (!$cliente->setNombres($_POST['nombres'])) {
-                    $result['exception'] = 'Nombres incorrectos';
-                } elseif (!$cliente->setApellidos($_POST['apellidos'])) {
-                    $result['exception'] = 'Apellidos incorrectos';
-                } elseif (!$cliente->setDUI($_POST['DUI'])) {
-                    $result['exception'] = 'DUI incorrecto';
-                } elseif (!$cliente->setCorreo($_POST['correo'])) {
-                    $result['exception'] = 'Correo incorrecto';
-                } elseif (!$cliente->setTelefono($_POST['telefono'])) {
-                    $result['exception'] = 'Teléfono incorrecto';
-                } elseif (!$cliente->setNacimiento($_POST['nacimiento'])) {
-                    $result['exception'] = 'Nacimiento incorrecto';
-                } elseif (!$cliente->setDireccion($_POST['direccion'])) {
-                    $result['exception'] = 'Dirección incorrecta';
-                } elseif ($cliente->updateRow()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Cliente actualizado correctamente';
-                } else {
-                    $result['exception'] = Database::getException();
-                }
-                break;
             case 'delete':
-                if (!$valoracion->setId($_POST['id_valoracion'])) {
+                if (!$valoracion->setId($_POST['id'])) {
                     $result['exception'] = 'Valoracion incorrecta';
                 } elseif (!$valoracion->readOne()) {
                     $result['exception'] = 'Valoracion inexistente';
@@ -177,7 +69,7 @@ if (isset($_GET['action'])) {
                 }
                 break;
             case 'changeStatus':
-                    if (!$valoracion->setId($_POST['id_valoracion'])) {
+                    if (!$valoracion->setId($_POST['id'])) {
                         $result['exception'] = 'Valoracion incorrecta';
                     } elseif (!$data = $valoracion->readOne()) {
                         $result['exception'] = 'Valoracion inexistente';
@@ -191,55 +83,7 @@ if (isset($_GET['action'])) {
             default:
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
-    } else {
-        // Se compara la acción a realizar cuando el administrador no ha iniciado sesión.
-        switch ($_GET['action']) {
-            case 'readUsers':
-                if ($usuario->readAll()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Debe autenticarse para ingresar';
-                } else {
-                    $result['exception'] = 'Debe crear un usuario para comenzar';
-                }
-                break;
-            case 'signup':
-                $_POST = Validator::validateForm($_POST);
-                if (!$usuario->setNombres($_POST['nombres'])) {
-                    $result['exception'] = 'Nombres incorrectos';
-                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
-                    $result['exception'] = 'Apellidos incorrectos';
-                } elseif (!$usuario->setCorreo($_POST['correo'])) {
-                    $result['exception'] = 'Correo incorrecto';
-                } elseif (!$usuario->setAlias($_POST['usuario'])) {
-                    $result['exception'] = 'Alias incorrecto';
-                } elseif ($_POST['codigo'] != $_POST['confirmar']) {
-                    $result['exception'] = 'Claves diferentes';
-                } elseif (!$usuario->setClave($_POST['codigo'])) {
-                    $result['exception'] = Validator::getPasswordError();
-                } elseif ($usuario->createRow()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Usuario registrado correctamente';
-                } else {
-                    $result['exception'] = Database::getException();
-                }
-                break;
-            case 'login':
-                $_POST = Validator::validateForm($_POST);
-                if (!$usuario->checkUser($_POST['alias'])) {
-                    $result['exception'] = 'Alias incorrecto';
-                } elseif ($usuario->checkPassword($_POST['clave'])) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Autenticación correcta';
-                    $_SESSION['id_admin'] = $usuario->getId();
-                    $_SESSION['alias_admin'] = $usuario->getAlias();
-                } else {
-                    $result['exception'] = 'Clave incorrecta';
-                }
-                break;
-            default:
-                $result['exception'] = 'Acción no disponible fuera de la sesión';
-        }
-    }
+    } 
     // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
     header('content-type: application/json; charset=utf-8');
     // Se imprime el resultado en formato JSON y se retorna al controlador.
--- controllers/dashboard/valoracion.js
+++ controllers/dashboard/valoracion.js
@@ -2,16 +2,9 @@
 const VALORACION_API = 'business/dashboard/valoracion.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('search-form');
-// Constante para establecer el formulario de guardar.
-const SAVE_FORM = document.getElementById('save-form');
-// Constante para establecer el título de la modal.
-const MODAL_TITLE = document.getElementById('modal-title');
 // Constantes para establecer el contenido de la tabla.
 const TBODY_ROWS = document.getElementById('tbody-rows');
 const RECORDS = document.getElementById('records');
-
-// Constante para establecer la modal de guardar.
-const MODAL = new bootstrap.Modal(document.getElementById('staticBackdrop'));
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -27,27 +20,6 @@ SEARCH_FORM.addEventListener('submit', (event) => {
     const FORM = new FormData(SEARCH_FORM);
     // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
     fillTable(FORM);
-});
-
-// Método manejador de eventos para cuando se envía el formulario de guardar.
-SAVE_FORM.addEventListener('submit', async (event) => {
-    // Se evita recargar la página web después de enviar el formulario.
-    event.preventDefault();
-    // Se verifica la acción a realizar.
-    (document.getElementById('id').value) ? action = 'update' : action = 'create';
-    // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(SAVE_FORM);
-    // Petición para guardar los datos del formulario.
-    const JSON = await dataFetch(VALORACION_API, action, FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se carga nuevamente la tabla para visualizar los cambios.
-        fillTable();
-        // Se muestra un mensaje de éxito.
-        sweetAlert(1, JSON.message, true);
-    } else {
-        sweetAlert(2, JSON.exception, false);
-    }
 });
 
 /*
@@ -71,7 +43,6 @@ async function fillTable(form = null) {
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TBODY_ROWS.innerHTML += `
                 <tr>
-                    <td>${row.id_producto}</td>
                     <td>${row.calificacion_producto}</td>
                     <td>${row.comentario_producto}</td>
                     <td>${row.fecha_comentario}</td>
@@ -97,67 +68,6 @@ async function fillTable(form = null) {
         RECORDS.textContent = JSON.message;
     } else {
         sweetAlert(4, JSON.exception, true);
-    }
-}
-
-/*
-*   Función para preparar el formulario al momento de insertar un registro.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-function openCreate() {
-    // Se restauran los elementos del formulario.
-    SAVE_FORM.reset();
-    // Se asigna título a la caja de diálogo.
-    MODAL_TITLE.textContent = 'Crear cliente';
-    // Se habilitan los campos necesarios.
-    document.getElementById('user').disabled = false;
-    document.getElementById('clave').disabled = false;
-    document.getElementById('confirmar').disabled = false;
-}
-
-/*
-*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
-async function openUpdate(id) {
-    // Se define una constante tipo objeto con los datos del registro seleccionado.
-    const FORM = new FormData();
-    FORM.append('id_valoracion', id);
-    // Petición para obtener los datos del registro solicitado.
-    const JSON = await dataFetch(VALORACION_API, 'readOne', FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se abre la caja de diálogo que contiene el formulario.
-        MODAL.show()
-        // Se restauran los elementos del formulario.
-        SAVE_FORM.reset();
-        // Se asigna título a la caja de diálogo.
-        MODAL_TITLE.textContent = 'Datos del cliente';
-        // Se deshabilitan los campos necesarios.
-        document.getElementById('nombres').disabled = true;
-        document.getElementById('apellidos').disabled = true;
-        document.getElementById('DUI').disabled = true;
-        document.getElementById('correo').disabled = true;
-        document.getElementById('telefono').disabled = true;
-        document.getElementById('nacimiento').disabled = true;
-        document.getElementById('direccion').disabled = true;
-        document.getElementById('user').disabled = true;
-        document.getElementById('clave').disabled = true;
-        document.getElementById('confirmar').disabled = true;
-        // Se inicializan los campos del formulario.
-        document.getElementById('id').value = JSON.dataset.id_cliente;
-        document.getElementById('nombres').value = JSON.dataset.nombre_cliente;
-        document.getElementById('apellidos').value = JSON.dataset.apellido_cliente;
-        document.getElementById('DUI').value = JSON.dataset.dui_cliente;
-        document.getElementById('correo').value = JSON.dataset.correo_cliente;
-        document.getElementById('telefono').value = JSON.dataset.telefono_cliente;
-        document.getElementById('nacimiento').value = JSON.dataset.nacimiento_cliente;
-        document.getElementById('direccion').value = JSON.dataset.direccion_cliente;
-        document.getElementById('user').value = JSON.dataset.user_cliente;
-    } else {
-        sweetAlert(2, JSON.exception, false);
     }
 }
 
