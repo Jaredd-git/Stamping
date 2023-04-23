ed7dd3bae442bd3c3b155fa84e7b4d59446edc67
Alejandro Granados <alehm0818@gmail.com>
2023-04-23 17:42:03 -0600

    cambios en pedidos

---
+++ api/entities/dao/detallepedido_queries.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../../helpers/database.php');
+/*
+*	Clase para manejar el acceso a datos de las entidades PEDIDO y DETALLE_PEDIDO.
+*/
+class DetalleQueries
+{
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+    public function readAll()
+    {
+        $sql = 'SELECT id_pedido, id_cliente, id_estado, fecha_pedido, direccion_pedido 
+        FROM pedidos
+        ORDER BY fecha_pedido DESC';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT p.id_pedido, c.nombre_cliente, e.estado, p.fecha_pedido, p.direccion_pedido 
+        FROM pedidos p
+        JOIN clientes c ON p.id_cliente = c.id_cliente 
+        JOIN estados_pedidos e ON p.id_estado = e.id_estado_pedido 
+		    WHERE id_pedido = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function changeStatus($status)
+    {
+        ($status) ? $status = 0 : $status = 1;
+        $sql = 'UPDATE clientes
+                SET estado_cliente = ?
+                WHERE id_cliente = ?';
+        $params = array($status, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+}
--- api/entities/dto/pedido.php
+++ api/entities/dto/pedido.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../../helpers/validator.php');
-require_once('../../entities/dao/pedido_queries.php');
+require_once('../../entities/dao/detallepedido_queries.php');
 /*
 *	Clase para manejar la transferencia de datos de las entidades PEDIDO y DETALLE_PEDIDO.
 */
