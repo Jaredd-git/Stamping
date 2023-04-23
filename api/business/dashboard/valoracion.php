3ea3334822e47ef1c81ec15f28395738f77cff70
Jaredd-git <20210612@ricaldone.edu.sv>
2023-04-23 16:02:39 -0600

    modificaciones

---
+++ api/business/dashboard/valoracion.php
@@ -0,0 +1,141 @@
+<?php
+require_once('../../entities/dto/valoracion.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $valoraciones = new Valoraciones;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'session' => 0, 'message' => null, 'exception' => null, 'dataset' => null, 'username' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['id_admin'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'getUser':
+                if (isset($_SESSION['alias_admin'])) {
+                    $result['status'] = 1;
+                    $result['username'] = $_SESSION['alias_admin'];
+                } else {
+                    $result['exception'] = 'Alias de usuario indefinido';
+                }
+                break;
+            case 'readAll':
+                if ($result['dataset'] = $valoraciones->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen '.count($result['dataset']).' registros';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            case 'search':
+                $_POST = Validator::validateForm($_POST);
+                if ($_POST['search'] == '') {
+                    $result['exception'] = 'Ingrese un valor para buscar';
+                } elseif ($result['dataset'] = $valoraciones->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                break;
+            case 'readOne':
+                if (!$valoraciones->setId($_POST['id_valoracion'])) {
+                    $result['exception'] = 'Valoracion incorrecta';
+                } elseif ($result['dataset'] = $valoraciones->readOne()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Valoracion inexistente';
+                }
+                break;
+            case 'delete':
+                if (!$valoraciones->setId($_POST['id_valoracion'])) {
+                    $result['exception'] = 'Valoracion incorrecta';
+                } elseif (!$valoraciones->readOne()) {
+                    $result['exception'] = 'Valoracion inexistente';
+                } elseif ($valoraciones->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Valoracion eliminada correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'changeStatus':
+                    if (!$valoraciones->setId($_POST['id_valoracion'])) {
+                        $result['exception'] = 'Valoracion incorrecta';
+                    } elseif (!$data = $valoraciones->readOne()) {
+                        $result['exception'] = 'Valoracion inexistente';
+                    } elseif ($valoraciones->changeStatus($data['estado_comentario'])) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Estado actualizado correctamente';
+                    } else {
+                        $result['exception'] = Database::getException();
+                    }
+                    break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        // Se compara la acción a realizar cuando el administrador no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readUsers':
+                if ($usuario->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Debe autenticarse para ingresar';
+                } else {
+                    $result['exception'] = 'Debe crear un usuario para comenzar';
+                }
+                break;
+            case 'signup':
+                $_POST = Validator::validateForm($_POST);
+                if (!$usuario->setNombres($_POST['nombres'])) {
+                    $result['exception'] = 'Nombres incorrectos';
+                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
+                    $result['exception'] = 'Apellidos incorrectos';
+                } elseif (!$usuario->setCorreo($_POST['correo'])) {
+                    $result['exception'] = 'Correo incorrecto';
+                } elseif (!$usuario->setAlias($_POST['usuario'])) {
+                    $result['exception'] = 'Alias incorrecto';
+                } elseif ($_POST['codigo'] != $_POST['confirmar']) {
+                    $result['exception'] = 'Claves diferentes';
+                } elseif (!$usuario->setClave($_POST['codigo'])) {
+                    $result['exception'] = Validator::getPasswordError();
+                } elseif ($usuario->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Usuario registrado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'login':
+                $_POST = Validator::validateForm($_POST);
+                if (!$usuario->checkUser($_POST['alias'])) {
+                    $result['exception'] = 'Alias incorrecto';
+                } elseif ($usuario->checkPassword($_POST['clave'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Autenticación correcta';
+                    $_SESSION['id_admin'] = $usuario->getId();
+                    $_SESSION['alias_admin'] = $usuario->getAlias();
+                } else {
+                    $result['exception'] = 'Clave incorrecta';
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible fuera de la sesión';
+        }
+    }
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
