04baef2605c7d502a1d85499638fd063884aeb2c
Jaredd-git <20210612@ricaldone.edu.sv>
2023-04-23 14:26:43 -0600

    Queries de crud de valoraciones

--- api/entities/dao/valoracion_queries.php
+++ api/entities/dao/valoracion_queries.php
@@ -10,54 +10,46 @@ class ValoracionQueries
     */
     public function searchRows($value)
     {
-        $sql = 'SELECT id_valoracion, calificacion_producto, comentario_producto, fecha_comentario, estado_comentario
-                FROM valoraciones
+        $sql = 'SELECT id_valoracion, id_producto, calificacion_producto, comentario_producto, fecha_comentario, estado_comentario
+                FROM valoraciones INNER JOIN productos USING(id_producto)
                 WHERE calificacion_producto ILIKE ? OR fecha_comentario ILIKE ?
-                ORDER BY nombre_producto';
+                ORDER BY calificacion_producto';
         $params = array("%$value%", "%$value%");
         return Database::getRows($sql, $params);
     }
 
-    public function createRow()
-    {
-        $sql = 'INSERT INTO productos(nombre_producto, descripcion_producto, precio_producto, estado_producto, color_producto, id_tipo, id_talla, existencias, imagen_producto, id_usuario)
-                VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $params = array($this->nombre, $this->descripcion, $this->precio, $this->estado, $this->color, $this->tipo, $this->talla, $this->existencias, $this->imagen, $_SESSION['id_admin']);
-        return Database::executeRow($sql, $params);
-    }
-
     public function readAll()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, estado_producto, color_producto, id_tipo, id_talla, existencias
-        FROM productos INNER JOIN tipos USING(id_tipo)
-        INNER JOIN tallas USING(id_talla)
-        ORDER BY nombre_producto';
+        $sql = 'SELECT id_valoracion, id_producto, calificacion_producto, comentario_producto, fecha_comentario, estado_comentario
+        FROM valoraciones INNER JOIN productos USING(id_producto)
+        ORDER BY calificacion_producto';
         return Database::getRows($sql);
     }
 
     public function readOne()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion_producto, precio_producto, estado_producto, color_producto, id_tipo, id_talla, existencias
-                FROM productos
-                WHERE id_producto = ?';
+        $sql = 'SELECT id_valoracion, id_producto, calificacion_producto, comentario_producto, fecha_comentario, estado_comentario
+                FROM valoraciones
+                WHERE id_valoracion = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
     }
 
-    public function updateRow()
+    public function deleteRow()
     {
-        $sql = 'UPDATE productos
-                SET nombre_producto = ?, descripcion_producto = ?, precio_producto = ?, estado_producto = ?, color_producto = ?, id_tipo = ?, id_talla = ?, existencias = ?
-                WHERE id_producto = ?';
-        $params = array($this->nombre, $this->descripcion, $this->precio, $this->estado, $this->color, $this->tipo, $this->talla, $this->id, $this->id);
+        $sql = 'DELETE FROM valoraciones
+                WHERE id_valoracion = ?';
+        $params = array($this->id);
         return Database::executeRow($sql, $params);
     }
 
-    public function deleteRow()
+    public function changeStatus($status)
     {
-        $sql = 'DELETE FROM productos
-                WHERE id_producto = ?';
-        $params = array($this->id);
+        ($status) ? $status = 0 : $status = 1;
+        $sql = 'UPDATE valoraciones
+                SET estado_comentario = ?
+                WHERE id_valoracion = ?';
+        $params = array($status, $this->id);
         return Database::executeRow($sql, $params);
     }
 }
