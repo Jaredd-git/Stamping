69326a6b0f71c6f15f962b42c8272df5f23ae224
Jaredd-git <20210612@ricaldone.edu.sv>
2023-04-23 19:09:10 -0600

    cambios

--- controllers/dashboard/valoracion.js
+++ controllers/dashboard/valoracion.js
@@ -1,10 +1,17 @@
 // Constante para completar la ruta de la API.
-const VALORACION_API = 'business/dashboard/valoracion.php';
+const VALORACIONES_API = 'business/dashboard/valoracion.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constante para establecer el título de la modal.
+const MODAL_TITLE = document.getElementById('modal-title');
 // Constantes para establecer el contenido de la tabla.
 const TBODY_ROWS = document.getElementById('tbody-rows');
 const RECORDS = document.getElementById('records');
+
+// Constante para establecer la modal de guardar.
+const MODAL = new bootstrap.Modal(document.getElementById('staticBackdrop'));
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -22,6 +29,27 @@ SEARCH_FORM.addEventListener('submit', (event) => {
     fillTable(FORM);
 });
 
+// Método manejador de eventos para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (document.getElementById('id').value) ? action = 'update' : action = 'create';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const JSON = await dataFetch(VALORACIONES_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, JSON.message, true);
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
+
 /*
 *   Función asíncrona para llenar la tabla con los registros disponibles.
 *   Parámetros: form (objeto opcional con los datos de búsqueda).
@@ -34,7 +62,7 @@ async function fillTable(form = null) {
     // Se verifica la acción a realizar.
     (form) ? action = 'search' : action = 'readAll';
     // Petición para obtener los registros disponibles.
-    const JSON = await dataFetch(VALORACION_API, action, form);
+    const JSON = await dataFetch(VALORACIONES_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se recorre el conjunto de registros fila por fila.
@@ -73,13 +101,74 @@ async function fillTable(form = null) {
 }
 
 /*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function openCreate() {
+    // Se restauran los elementos del formulario.
+    SAVE_FORM.reset();
+    // Se asigna título a la caja de diálogo.
+    MODAL_TITLE.textContent = 'Crear cliente';
+    // Se habilitan los campos necesarios.
+    document.getElementById('user').disabled = false;
+    document.getElementById('clave').disabled = false;
+    document.getElementById('confirmar').disabled = false;
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openUpdate(id) {
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('id_valoracion', id);
+    // Petición para obtener los datos del registro solicitado.
+    const JSON = await dataFetch(VALORACION_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se abre la caja de diálogo que contiene el formulario.
+        MODAL.show()
+        // Se restauran los elementos del formulario.
+        SAVE_FORM.reset();
+        // Se asigna título a la caja de diálogo.
+        MODAL_TITLE.textContent = 'Datos del cliente';
+        // Se deshabilitan los campos necesarios.
+        document.getElementById('nombres').disabled = true;
+        document.getElementById('apellidos').disabled = true;
+        document.getElementById('DUI').disabled = true;
+        document.getElementById('correo').disabled = true;
+        document.getElementById('telefono').disabled = true;
+        document.getElementById('nacimiento').disabled = true;
+        document.getElementById('direccion').disabled = true;
+        document.getElementById('user').disabled = true;
+        document.getElementById('clave').disabled = true;
+        document.getElementById('confirmar').disabled = true;
+        // Se inicializan los campos del formulario.
+        document.getElementById('id').value = JSON.dataset.id_cliente;
+        document.getElementById('nombres').value = JSON.dataset.nombre_cliente;
+        document.getElementById('apellidos').value = JSON.dataset.apellido_cliente;
+        document.getElementById('DUI').value = JSON.dataset.dui_cliente;
+        document.getElementById('correo').value = JSON.dataset.correo_cliente;
+        document.getElementById('telefono').value = JSON.dataset.telefono_cliente;
+        document.getElementById('nacimiento').value = JSON.dataset.nacimiento_cliente;
+        document.getElementById('direccion').value = JSON.dataset.direccion_cliente;
+        document.getElementById('user').value = JSON.dataset.user_cliente;
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+}
+
+/*
 *   Función asíncrona para eliminar un registro.
 *   Parámetros: id (identificador del registro seleccionado).
 *   Retorno: ninguno.
 */
 async function openDelete(id) {
     // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar la valoración?');
+    const RESPONSE = await confirmAction('¿Desea eliminar el cliente?');
     // Se verifica la respuesta del mensaje.
     if (RESPONSE) {
         // Se define una constante tipo objeto con los datos del registro seleccionado.
@@ -106,7 +195,7 @@ async function openDelete(id) {
 */
 async function openChangeStatus(id) {
     // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea cambiar el estado de la valoracion?');
+    const RESPONSE = await confirmAction('¿Desea cambiar el estado del cliente?');
     // Se verifica la respuesta del mensaje.
     if (RESPONSE) {
         const FORM = new FormData();
