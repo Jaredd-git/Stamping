6a5a59a72d3f859027489ab051fe761f1a050047
Alejandro Granados <alehm0818@gmail.com>
2023-04-29 23:50:38 -0600

    Agregar opción para mostrar/ocultar la contraseña en formulario de inicio de sesión
    Agrega funcionalidad al formulario de inicio de sesión al permitir al usuario mostrar u ocultar la contraseña ingresada en el campo correspondiente. Esto se logra mediante la creación de un evento en el botón "Mostrar contraseña" que cambia el tipo de input del campo de contraseña entre "password" y "text", y cambia el icono del botón para indicar el estado actual de la contraseña.
    
    Además, se ha agregado un tooltip de Bootstrap para proporcionar información adicional al usuario sobre la función del botón de mostrar/ocultar contraseña.
    
    También incluye algunos cambios menores en la configuración del tooltip para ajustar la velocidad de aparición y desaparición de la información.

--- controllers/dashboard/index.js
+++ controllers/dashboard/index.js
@@ -1,23 +1,32 @@
 // Constante para establecer el formulario de inicio de sesión.
 const LOGIN_FORM = document.getElementById('login-form');
 
+/**
+ * Event listener que se ejecuta cuando el DOM ha sido cargado.
+ * Se encarga de obtener los elementos necesarios para mostrar/ocultar la contraseña y asignar un evento al botón correspondiente.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const showPasswordButton = document.querySelector('#show-password');
     const passwordInput = document.querySelector('#pass');
   
+    // Se crea una instancia del tooltip de Bootstrap y se asignan las opciones.
     if (showPasswordButton) {
         const tooltip = new bootstrap.Tooltip(showPasswordButton, {
         title: 'Mostrar contraseña',
         placement: 'bottom',
-        delay: { show: 0, hide: 100 }
+        // Retraso para mostrar y ocultar la información
+        delay: { show: 0, hide: 1 }
     });
   
+    // Se asigna un evento al botón para cambiar el tipo de input y mostrar/ocultar la contraseña.
     showPasswordButton.addEventListener('click', () => {
+        // Se verifica si el tipo de atributo "type" del elemento passwordInput es "password". Si es así, se establece el valor "text" a la variable type, de lo contrario se establece el valor "password".
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
         passwordInput.setAttribute('type', type);
+        // Cambia el ícono del botón de mostrar/ocultar contraseña
         showPasswordButton.querySelector('i').classList.toggle('bi-eye-fill');
         showPasswordButton.querySelector('i').classList.toggle('bi-eye-slash-fill');
-  
+        // Actualizar el título del tooltip según el estado actual del botón
         if (showPasswordButton.querySelector('i').classList.contains('bi-eye-slash-fill')) {
             tooltip._config.title = 'Ocultar contraseña';
             tooltip.show();
