6b9876846ab0c60fa95b6c07690d50a0223b36cb
ale080518 <alehm0818@gmail.com>
2023-05-04 15:26:43 -0600

    Actualización de botón para abrir el modal de detalle
    Se cambia el icono y el color del botón

--- controllers/dashboard/pedido.js
+++ controllers/dashboard/pedido.js
@@ -82,8 +82,8 @@ async function fillTable(form = null) {
                     <td>${row.fecha_pedido}</td>
                     <td>${row.direccion_pedido}</td>
                     <th>
-                        <button  onclick="openUpdate(${row.id_pedido})" class="btn btn-outline-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Cambiar estado del pedido">
-                            <i class="bi bi-file-text"></i>
+                        <button  onclick="openUpdate(${row.id_pedido})" class="btn btn-outline-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Cambiar estado del pedido">
+                            <i class="bi bi-check2-square"></i>
                         </button>
                         <button  onclick="openDetail(${row.id_pedido})" class="btn btn-outline-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ver detalle del pedido">
                             <i class="bi bi-file-text"></i>
