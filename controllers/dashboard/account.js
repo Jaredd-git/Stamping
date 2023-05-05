7ea7c360b684804406246c2ba9f007b2535b8e70
Alejandro Granados <alehm0818@gmail.com>
2023-05-04 20:15:07 -0600

    Refactorización de account

--- controllers/dashboard/account.js
+++ controllers/dashboard/account.js
@@ -1,4 +1,3 @@
-
 const USER_API = 'business/dashboard/usuario.php';
 // Constantes para establecer las etiquetas de encabezado y pie de la página web.
 const HEADER = document.querySelector('header');
@@ -12,44 +11,43 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (JSON.session) {
         if (JSON.status) {
             HEADER.innerHTML = `
-          <!-- Navbar de la pagina web -->
-          <div>
+            <!-- Navbar de la pagina web -->
+            <div>
                 <nav class="navbar-ligth sticky-top" style="background-color: #ccc;">
                     <div class="text-center p-4" >
-                    <a style="text-decoration: none;" class="text-reset fw-bold" href="home.html">STAMPING</a>
-                  </div>
+                        <a style="text-decoration: none;" class="text-reset fw-bold" href="home.html">STAMPING</a>
+                    </div>
                 </nav>  
                 <div class="dropdown">
-                    <button class="btn-secondary dropdown-toggle mt-3 ms-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    Cuenta: <b>${JSON.username}</b>
+                    <button class="btn-secondary dropdown-toggle mt-3 ms-3" type="button"id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cuenta: <b>${JSON.username}</b>
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                      <li><a class="dropdown-item" onClick="logOut()">Salir</a></li>
+                        <li><a class="dropdown-item" onClick="logOut()">Salir</a></li>
                     </ul>
                 </div>         
-          </div>
-          `;
+            </div>
+            `;
 
             /* Se manda al footer codigo para usarlo en el html */
             FOOTER.innerHTML = `
-          <!-- Copyright -->
-          <div style="background-color: rgba(0, 0, 0, 0.05); ">
-            © 2023 Copyright:
-            <a class="text-reset fw-bold" style="text-decoration: none;" href="https://us-tuna-sounds-images.voicemod.net/88463959-b331-4a6a-b9ee-9843ec1a9229-1665760464118.png">Stamping.com</a>
-          </div>
-          <!-- Copyright -->
+            <!-- Copyright -->
+            <div style="background-color: rgba(0, 0, 0, 0.05); ">
+                © 2023 Copyright:
+                <a class="text-reset fw-bold" style="text-decoration: none;" href="https://us-tuna-sounds-images.voicemod.net/88463959-b331-4a6a-b9ee-9843ec1a9229-1665760464118.png">Stamping.com</a>
+            </div>
+            <!-- Copyright -->
             `;
         } else {
 
-
             /* Se manda al footer codigo para usarlo en el html */
             FOOTER.innerHTML = `
-          <!-- Copyright -->
-          <div style="background-color: rgba(0, 0, 0, 0.05); ">
-            © 2023 Copyright:
-            <a class="text-reset fw-bold" style="text-decoration: none;" href="https://us-tuna-sounds-images.voicemod.net/88463959-b331-4a6a-b9ee-9843ec1a9229-1665760464118.png">Stamping.com</a>
-          </div>
-          <!-- Copyright -->
+            <!-- Copyright -->
+            <div style="background-color: rgba(0, 0, 0, 0.05); ">
+                © 2023 Copyright:
+                <a class="text-reset fw-bold" style="text-decoration: none;" href="https://us-tuna-sounds-images.voicemod.net/88463959-b331-4a6a-b9ee-9843ec1a9229-1665760464118.png">Stamping.com</a>
+            </div>
+            <!-- Copyright -->
             `;
         }
     }
