88ba6c34f3dc13769f1702c75a61b9551f6eca89
Deschavetado <20210612@ricaldone.edu.sv>
2023-05-11 15:28:18 -0600

    cambios
    cambios

--- api/entities/dao/tipo_queries.php
+++ api/entities/dao/tipo_queries.php
@@ -5,6 +5,18 @@ require_once('../../helpers/database.php');
 */
 class TipoQueries
 {
+    //Consulta para realizar la busqueda de datos en la tabla usuarios
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_tipo, nombre_tipo, descripcion_tipo
+                FROM tipos
+                WHERE nombre_tipo ILIKE ? OR descripcion_tipo ILIKE ?
+                ORDER BY nombre_tipo';
+                //Guarda en un array los parametros de busqueda
+        $params = array("%$value%", "%$value%");
+        //Devuelve los datos buscados
+        return Database::getRows($sql, $params);
+    }
     //Consulta para cargar todos los registros de la tabla tipos
     public function readAll()
     {
@@ -20,7 +32,7 @@ class TipoQueries
         $sql = 'INSERT INTO tipos(nombre_tipo, descripcion_tipo)
                 VALUES(?, ?)';
                 //Obtiene los datos del nuevo tipo 
-        $params = array($this->nombres, $this->descripcion);
+        $params = array($this->nombre, $this->descripcion);
         //Crea el usuario en la base de datos
         return Database::executeRow($sql, $params);
     }
@@ -37,12 +49,23 @@ class TipoQueries
     //Consulta para cargar un dato especifico de la tabla productos
     public function readOne()
     {
-        $sql = 'SELECT id_tipo nombre_tipo, descripcion_tipo
+        $sql = 'SELECT id_tipo, nombre_tipo, descripcion_tipo
                 FROM tipos
                 WHERE id_tipo = ?';
                 //Se obtiene el id del producto
         $params = array($this->id);
         //Carga el dato seleccionado
         return Database::getRow($sql, $params);
+    }
+    //Actualiza unos datos especificos de la tabla productos
+    public function updateRow()
+    {
+        $sql = 'UPDATE tipos
+                SET nombre_tipo = ?, descripcion_tipo = ?
+                WHERE id_tipo = ?';
+                //Obtiene todos los datos para la actualizacion del producto
+        $params = array($this->nombre, $this->descripcion, $this->id);
+        //Actualiza el producto seleccionado
+        return Database::executeRow($sql, $params);
     }
 }
