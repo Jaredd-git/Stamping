54ac9189f0afd73c43b8384ef1a356573a4129c4
Alejandro Granados <alehm0818@gmail.com>
2023-05-19 20:35:59 -0600

    Actualización de formato

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -9,34 +9,34 @@ const OPTIONS = {
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
-    // Petición para obtener las categorías disponibles.
-    const JSON = await dataFetch(PRODUCTO_API, 'readAllPreview');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se inicializa el contenedor de categorías.
-        PRODUCTOS.innerHTML = '';
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        JSON.dataset.forEach(row => {
-            // Se establece la página web de destino con los parámetros.
-            url = `../views/public/verprenda.html?id=${row.id_producto}&nombre=${row.nombre_producto}`;
-            // Se crean y concatenan las tarjetas con los datos de cada categoría.
-            PRODUCTOS.innerHTML += `
+  // Petición para obtener las categorías disponibles.
+  const JSON = await dataFetch(PRODUCTO_API, 'readAllPreview');
+  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+  if (JSON.status) {
+    // Se inicializa el contenedor de categorías.
+    PRODUCTOS.innerHTML = '';
+    // Se recorre el conjunto de registros fila por fila a través del objeto row.
+    JSON.dataset.forEach(row => {
+      // Se establece la página web de destino con los parámetros.
+      url = `../views/public/verprenda.html?id=${row.id_producto}&nombre=${row.nombre_producto}`;
+      // Se crean y concatenan las tarjetas con los datos de cada categoría.
+      PRODUCTOS.innerHTML += `
             <div class="col">
                 <div class="card text-light">
-                  <img src="${SERVER_URL}../images/productos/${row.imagen_producto}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">${row.nombre_producto}</h5>
-                    <p class="card-text">${row.descripcion_producto}</p>
-                    <p class="card-text">${row.precio_producto}</p>
-                    <a href="${url}" class="btn btn-primary">Ver producto</a>
-                  </div>
+                    <img src="${SERVER_URL}../images/productos/${row.imagen_producto}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${row.nombre_producto}</h5>
+                        <p class="card-text">${row.descripcion_producto}</p>
+                        <p class="card-text">${row.precio_producto}</p>
+                        <a href="${url}" class="btn btn-primary">Ver producto</a>
+                    </div>
                 </div>
-              </div>
+            </div>
             `;
-        });
-        
+    });
+
     } else {
-        // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
-        document.getElementById('title').textContent = JSON.exception;
+    // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
+    document.getElementById('title').textContent = JSON.exception;
     }
 });
