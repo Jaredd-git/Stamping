f05b066f9a68ea1a1405a091c9a1ea8143cbec22
Jaredd-git <20210612@ricaldone.edu.sv>
2023-05-21 17:19:13 -0600

    cambios

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -1,26 +1,29 @@
 // Constante para completar la ruta de la API.
-const PRODUCTO_API = 'business/public/producto.php'; 
+const PRODUCTO_API = 'business/public/producto.php';
 // Constante para establecer el contenedor de categorías.
 const PRODUCTOS = document.getElementById('productos');
 // Constante tipo objeto para establecer las opciones del componente Slider.
 const OPTIONS = {
-    height: 300
+  height: 300
 }
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
-    // Petición para obtener las categorías disponibles.
-    const JSON = await dataFetch(PRODUCTO_API, 'readAllPreview');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (JSON.status) {
-        // Se inicializa el contenedor de categorías.
-        PRODUCTOS.innerHTML = '';
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        JSON.dataset.forEach(row => {
-            PRODUCTOS.innerHTML += `
+  // Petición para obtener las categorías disponibles.
+  const JSON = await dataFetch(PRODUCTO_API, 'readAllPreview');
+  // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+  if (JSON.status) {
+    // Se inicializa el contenedor de categorías.
+    PRODUCTOS.innerHTML = '';
+    // Se recorre el conjunto de registros fila por fila a través del objeto row.
+    JSON.dataset.forEach(row => {
+      PRODUCTOS.innerHTML += `
             <div class="col">
                 <div class="card text-black">
                   <img src="${SERVER_URL}images/productos/${row.imagen_producto}" class="card-img-top" alt="...">
+                  <a href="verprenda.html?id=${row.id_producto}" class="btn btn-dark mt-2 ms-2 me-2" data-tooltip="Ver producto">
+                    Ver producto
+                  </a>
                   <div class="card-body">
                     <h5 class="card-title">${row.nombre_producto}</h5>
                     <p class="card-text">${row.descripcion_producto}</p>
@@ -30,9 +33,8 @@ document.addEventListener('DOMContentLoaded', async () => {
             </div>
             `;
     });
-
-    } else {
+  } else {
     // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
     document.getElementById('title').textContent = JSON.exception;
-    }
+  }
 });
