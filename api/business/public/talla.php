f6924f33f20ac7d3c319f522dd32b347911a26c8
aljo9898 <gar.v.alejandro@gmail.com>
2023-05-23 20:31:05 -0600

    cambios en verPrenda
    solo falta boton de agregar

--- api/business/public/talla.php
+++ api/business/public/talla.php
@@ -1,18 +1,16 @@
 <?php
-require_once('../../entities/dto/producto.php');
+require_once('../../entities/dto/talla.php');
 
 // Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
 if (isset($_GET['action'])) {
     // Se instancia la clase correspondiente.
-    $producto = new Producto;
+    $talla = new Talla;
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
-    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    
         switch ($_GET['action']) {
-            //Accion utilizada para poder leer todos los campos de la tabla productos
-            case 'readAllPreview':
-                if ($result['dataset'] = $producto->readAllPreview()) {
+            // Accion utilizada para leer los datos de la tabla tallas 
+            case 'readAll':
+                if ($result['dataset'] = $talla->readAll()) {
                     $result['status'] = 1;
                     $result['message'] = 'Existen '.count($result['dataset']).' registros';
                     // Si ocurre una excepción en la base de datos, se captura
@@ -23,29 +21,14 @@ if (isset($_GET['action'])) {
                     $result['exception'] = 'No hay datos registrados';
                 }
                 break;
-            case 'readOne':
-                // Se comprueba si se ha ingresado correctamente el ID del producto
-                if (!$producto->setId($_POST['id_producto'])) {
-                    $result['exception'] = 'Producto incorrecto';
-                    // Si se encuentra el producto, se muestra en la respuesta
-                } elseif ($result['dataset'] = $producto->readOne()) {
-                    $result['status'] = 1;
-                    // Si ocurre una excepción en la base de datos, se captura
-                } elseif (Database::getException()) {
-                    $result['exception'] = Database::getException();
-                    // Si el producto no existe, se informa al usuario
-                } else {
-                    $result['exception'] = 'Producto inexistente';
-                }
-                break;
             default:
-            // Si no se encuentra ninguna acción disponible dentro de la sesión, se muestra un mensaje de error
                 $result['exception'] = 'Acción no disponible dentro de la sesión';
         }
         // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
         header('content-type: application/json; charset=utf-8');
         // Se imprime el resultado en formato JSON y se retorna al controlador.
         print(json_encode($result));
+    
 } else {
     print(json_encode('Recurso no disponible'));
 }
