d1bf2b59bc374df9f23b30dc13daea19fff69dfb
Alejandro Granados <alehm0818@gmail.com>
2023-07-25 22:05:24 -0600

    Actualización de gráfico

--- controllers/dashboard/graficos.js
+++ controllers/dashboard/graficos.js
@@ -1,5 +1,5 @@
 // Constante para completar la ruta de la API.
-const PRODUCTO_API = 'business/dashboard/producto.php';
+const CLIENTE_API = 'business/dashboard/cliente.php';
 
 // Método manejador de eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,33 +20,33 @@ document.addEventListener('DOMContentLoaded', () => {
     // Se muestra un saludo en la página web.
     document.getElementById('greeting').textContent = greeting;
     // Se llaman a la funciones que generan los gráficos en la página web.
-    graficoBarrasCategorias();
+    graficoBarrasEstado();
     graficoPastelCategorias();
 });
 
 /*
-*   Función asíncrona para mostrar en un gráfico de barras la cantidad de productos por categoría.
+*   Función asíncrona para mostrar en un gráfico de barras que muestra la cantidad de clientes por estado.
 *   Parámetros: ninguno.
 *   Retorno: ninguno.
 */
-async function graficoBarrasCategorias() {
+async function graficoBarrasEstado() {
     // Petición para obtener los datos del gráfico.
-    const DATA = await dataFetch(PRODUCTO_API, 'cantidadProductosCategoria');
+    const DATA = await dataFetch(CLIENTE_API, 'clientesActivosInactivos');
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
     if (DATA.status) {
         // Se declaran los arreglos para guardar los datos a graficar.
-        let categorias = [];
+        let estados = [];
         let cantidades = [];
         // Se recorre el conjunto de registros fila por fila a través del objeto row.
         DATA.dataset.forEach(row => {
             // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
+            estados.push(row.estado_cliente);
             cantidades.push(row.cantidad);
         });
         // Llamada a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+        barGraph('chart5', estados, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
     } else {
-        document.getElementById('chart1').remove();
+        document.getElementById('chart5').remove();
         console.log(DATA.exception);
     }
 }
