73c6d8e7c50de00e8c93581bad098124f5b4ca7f
Alejandro Granados <alehm0818@gmail.com>
2023-07-25 22:35:36 -0600

    Actualización de gráficos

--- controllers/dashboard/graficos.js
+++ controllers/dashboard/graficos.js
@@ -21,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('greeting').textContent = greeting;
     // Se llaman a la funciones que generan los gráficos en la página web.
     graficoBarrasEstado();
-    graficoPastelCategorias();
 });
 
 /*
@@ -44,36 +43,9 @@ async function graficoBarrasEstado() {
             cantidades.push(row.cantidad);
         });
         // Llamada a la función que genera y muestra un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart5', estados, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
+        barGraph('chart5', estados, cantidades, 'Cantidad de productos', 'Clientes por estado');
     } else {
         document.getElementById('chart5').remove();
-        console.log(DATA.exception);
-    }
-}
-
-/*
-*   Función asíncrona para mostrar en un gráfico de pastel el porcentaje de productos por categoría.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-async function graficoPastelCategorias() {
-    // Petición para obtener los datos del gráfico.
-    const DATA = await dataFetch(PRODUCTO_API, 'porcentajeProductosCategoria');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
-    if (DATA.status) {
-        // Se declaran los arreglos para guardar los datos a gráficar.
-        let categorias = [];
-        let porcentajes = [];
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        DATA.dataset.forEach(row => {
-            // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
-            porcentajes.push(row.porcentaje);
-        });
-        // Llamada a la función que genera y muestra un gráfico de pastel. Se encuentra en el archivo components.js
-        pieGraph('chart2', categorias, porcentajes, 'Porcentaje de productos por categoría');
-    } else {
-        document.getElementById('chart2').remove();
         console.log(DATA.exception);
     }
 }
