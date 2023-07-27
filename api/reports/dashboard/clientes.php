08f53944f24437a732b691e745ea6b3d989839e3
Jaredd-git <20210612@ricaldone.edu.sv>
2023-07-27 00:26:42 -0600

    reportes

---
+++ api/reports/dashboard/clientes.php
@@ -0,0 +1,39 @@
+<?php
+// Se incluye la clase con las plantillas para generar reportes.
+require_once('../../helpers/report.php');
+// Se incluyen las clases para la transferencia y acceso a datos.
+require_once('../../entities/dto/cliente.php');
+
+// Se instancia la clase para crear el reporte.
+$pdf = new Report;
+// Se inicia el reporte con el encabezado del documento.
+$pdf->startReport('Clientes activos e inactivos');
+// Se instancia el módelo Categoría para obtener los datos.
+$cliente = new Cliente;
+// Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
+if ($dataClientes = $cliente->readAll()) {
+    // Se establece un color de relleno para los encabezados.
+
+    $pdf->setFillColor(43, 110, 181);
+    // Se establece la fuente para los encabezados.
+    $pdf->setFont('Arial', 'B', 12);
+    // Se imprimen las celdas con los encabezados.
+    $pdf->cell(80, 10, 'Nombre', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Apellido', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Estado', 1, 0, 'C', 1);
+
+    // Se establece un color de relleno para mostrar el nombre de la categoría.
+    $pdf->setFillColor(225);
+    // Se establece la fuente para los datos de los productos.
+    $pdf->setFont('Arial', 'B', 10);
+
+    foreach ($dataClientes as $rowClientes) {
+        ($rowCliente['estado_cliente']) ? $estado = 'Activo' : $estado = 'Inactivo';
+        // Se imprimen las celdas con los datos de los productos.
+        $pdf->cell(80, 10, $pdf->encodeString($rowCliente['nombre_cliente']), 1, 0);
+        $pdf->cell(30, 10, $rowCliente['apellido_cliente'], 1, 0);
+        $pdf->cell(30, 10, $estado, 1, 0);}
+    } else {
+        $pdf->cell(0, 10, $pdf->encodeString('Producto incorrecta o inexistente'), 1, 1);
+    } 
+    $pdf->output('I', 'Clientes.pdf');
