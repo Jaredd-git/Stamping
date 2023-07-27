f4c9c61bb82a13d49d5b20629f0a585d88dda854
Alejandro Granados <alehm0818@gmail.com>
2023-07-27 14:19:57 -0600

    Update clientes.php

--- api/reports/dashboard/clientes.php
+++ api/reports/dashboard/clientes.php
@@ -7,19 +7,19 @@ require_once('../../entities/dto/cliente.php');
 // Se instancia la clase para crear el reporte.
 $pdf = new Report;
 // Se inicia el reporte con el encabezado del documento.
-$pdf->startReport('Estado Clientes');
+$pdf->startReport('Clientes por estado');
 // Se instancia el módelo Categoría para obtener los datos.
 $cliente = new Cliente;
 // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
 if ($dataClientes = $cliente->readAll()) {
     // Se establece un color de relleno para los encabezados.
 
-    $pdf->setFillColor(43, 110, 181);
+    $pdf->setFillColor(147, 203, 230);
     // Se establece la fuente para los encabezados.
     $pdf->setFont('Helvetica', 'B', 12);
     // Se imprimen las celdas con los encabezados.
-    $pdf->cell(40, 10, 'Nombre', 1, 0, 'C', 1);
-    $pdf->cell(30, 10, 'Apellido', 1, 0, 'C', 1);
+    $pdf->cell(78, 10, 'Nombre', 1, 0, 'C', 1);
+    $pdf->cell(78, 10, 'Apellido', 1, 0, 'C', 1);
     $pdf->cell(30, 10, 'Estado', 1, 1, 'C', 1);
 
     // Se establece un color de relleno para mostrar el nombre de la categoría.
@@ -30,8 +30,8 @@ if ($dataClientes = $cliente->readAll()) {
     foreach ($dataClientes as $rowClientes) {
         ($rowClientes['estado_cliente']) ? $estado = 'Activo' : $estado = 'Inactivo';
         // Se imprimen las celdas con los datos de los productos.
-        $pdf->cell(40, 10, $pdf->encodeString($rowClientes['nombre_cliente']), 1, 0, 'C', 1);
-        $pdf->cell(30, 10, $pdf->encodeString($rowClientes['apellido_cliente']), 1, 0, 'C', 1);
+        $pdf->cell(78, 10, $pdf->encodeString($rowClientes['nombre_cliente']), 1, 0, 'C', 1);
+        $pdf->cell(78, 10, $pdf->encodeString($rowClientes['apellido_cliente']), 1, 0, 'C', 1);
         $pdf->cell(30, 10, $estado, 1, 1, 'C', 1);}
     } else {
         $pdf->cell(0, 10, $pdf->encodeString('Producto incorrecta o inexistente'), 1, 1);
