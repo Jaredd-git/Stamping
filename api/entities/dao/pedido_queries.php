a86f883c6ba7f3bb7bd24901d643fbedf151eeee
Elver galarga <20210612@ricaldone.edu.sv>
2023-07-27 13:28:29 -0600

    reportes

--- api/entities/dao/pedido_queries.php
+++ api/entities/dao/pedido_queries.php
@@ -194,5 +194,15 @@ class PedidoQueries
         WHERE id_pedido = 2";
         return Database::getRows($sql);
     }
+
+    public function readAllPedidosE()
+    {
+        $sql = "SELECT id_pedido, CONCAT(nombre_cliente,' ', apellido_cliente) cliente, id_estado, estado, fecha_pedido, direccion_pedido
+        FROM pedidos p
+        INNER JOIN clientes USING(id_cliente)
+        INNER JOIN estados_pedidos ep ON ep.id_estado_pedido = p.id_estado
+        WHERE id_pedido = 1";
+        return Database::getRows($sql);
+    }
 }
 
--- api/reports/dashboard/pedidos.php
+++ api/reports/dashboard/pedidos.php
@@ -7,9 +7,64 @@ require_once('../../entities/dto/pedido.php');
 // Se instancia la clase para crear el reporte.
 $pdf = new Report;
 // Se inicia el reporte con el encabezado del documento.
-$pdf->startReport('Pedidos pendientes');
+$pdf->startReport('Pedidos');
 // Se instancia el módelo Categoría para obtener los datos.
 $pedido = new Pedido;
+
+if ($dataPedidos = $pedido->readAllPedidosE()) {
+    // Se establece un color de relleno para los encabezados.
+
+    $pdf->setFillColor(43, 110, 181);
+    // Se establece la fuente para los encabezados.
+    $pdf->setFont('Arial', 'B', 12);
+    // Se imprimen las celdas con los encabezados.
+    $pdf->cell(40, 10, 'Cliente', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Fecha', 1, 0, 'C', 1);
+    $pdf->cell(45, 10, 'Direccion', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Estado', 1, 1, 'C', 1);
+
+    // Se establece un color de relleno para mostrar el nombre de la categoría.
+    $pdf->setFillColor(188, 190, 204);
+    // Se establece la fuente para los datos de los productos.
+    $pdf->setFont('Arial', 'B', 10);
+
+    foreach ($dataPedidos as $rowPedido) {
+        // Se imprimen las celdas con los datos de los productos.
+        $pdf->cell(40, 10, $pdf->encodeString($rowPedido['cliente']), 1, 0, 'C', 1);
+        $pdf->cell(30, 10, $pdf->encodeString($rowPedido['fecha_pedido']), 1, 0, 'C', 1);
+        $pdf->cell(45, 10, $pdf->encodeString($rowPedido['direccion_pedido']), 1, 0, 'C', 1);
+        $pdf->cell(30, 10, ($rowPedido['estado']), 1, 1, 'C', 1);}
+    } else {
+        $pdf->cell(0, 10, $pdf->encodeString('Pedido incorrecta o inexistente'), 1, 1);
+    }
+
+if ($dataPedidos = $pedido->readAllPedidosC()) {
+    // Se establece un color de relleno para los encabezados.
+
+    $pdf->setFillColor(43, 110, 181);
+    // Se establece la fuente para los encabezados.
+    $pdf->setFont('Arial', 'B', 12);
+    // Se imprimen las celdas con los encabezados.
+    $pdf->cell(40, 10, 'Cliente', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Fecha', 1, 0, 'C', 1);
+    $pdf->cell(45, 10, 'Direccion', 1, 0, 'C', 1);
+    $pdf->cell(30, 10, 'Estado', 1, 1, 'C', 1);
+
+    // Se establece un color de relleno para mostrar el nombre de la categoría.
+    $pdf->setFillColor(188, 190, 204);
+    // Se establece la fuente para los datos de los productos.
+    $pdf->setFont('Arial', 'B', 10);
+
+    foreach ($dataPedidos as $rowPedido) {
+        // Se imprimen las celdas con los datos de los productos.
+        $pdf->cell(40, 10, $pdf->encodeString($rowPedido['cliente']), 1, 0, 'C', 1);
+        $pdf->cell(30, 10, $pdf->encodeString($rowPedido['fecha_pedido']), 1, 0, 'C', 1);
+        $pdf->cell(45, 10, $pdf->encodeString($rowPedido['direccion_pedido']), 1, 0, 'C', 1);
+        $pdf->cell(30, 10, ($rowPedido['estado']), 1, 1, 'C', 1);}
+    } else {
+        $pdf->cell(0, 10, $pdf->encodeString('Pedido incorrecta o inexistente'), 1, 1);
+    }
+
 // Se verifica si existen registros para mostrar, de lo contrario se imprime un mensaje.
 if ($dataPedidos = $pedido->readAllPedidosP()) {
     // Se establece un color de relleno para los encabezados.
@@ -37,4 +92,5 @@ if ($dataPedidos = $pedido->readAllPedidosP()) {
     } else {
         $pdf->cell(0, 10, $pdf->encodeString('Pedido incorrecta o inexistente'), 1, 1);
     } 
+
     $pdf->output('I', 'Pedidos.pdf');
